98bbc70e7d067a55931ca8020c57677e59ede639
Christian L <138773189+Znajkie@users.noreply.github.com>
2023-11-16 23:08:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+const charactersEl = document.getElementById('characters');
+const prev = document.getElementById('prev-button');
+const next = document.getElementById('next-button');
+const details = document.getElementById('details');
+const getCharacters = (url = 'https://swapi.dev/api/people') => {
+  const loaderEl = document.createElement('div');
+  loaderEl.innerText = 'Loading';
+  charactersEl.append(loaderEl);
+const header = document.createElement('h1');
+header.textContent = 'Details';
+details.append(header)
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      charactersEl.innerText = '';
+
+      responseJson.results.forEach((character) => {
+        const characterEl = document.createElement('p');
+
+        characterEl.addEventListener('click', () => {
+          
+          const detailsEl = document.createElement('p');
+      
+          details.innerText = '';
+          detailsEl.innerText = 
+          `Height: ${character.height}
+           Mass: ${character.mass}
+           Skin Color: ${character.skin_color}
+           Eye Color: ${character.eye_color}
+           Hair Color: ${character.hair_color}
+           Birth year: ${character.birth_year}
+           Gender: ${character.gender}`;
+          details.append(detailsEl)
+        })
+
+        characterEl.innerText = character.name;
+        charactersEl.append(characterEl);
+        console.log(character)
+      });
+
+      prev.addEventListener('click', () => {
+        charactersEl.innerText = '';
+        details.innerText = '';
+        getCharacters(responseJson.previous);
+      });
+      next.addEventListener('click', () => {
+        charactersEl.innerText = '';
+        details.innerText = '';
+        getCharacters(responseJson.next);
+      });
+
+      if (responseJson.previous) {
+        prev.disabled = false;
+      } else {
+        prev.disabled = true;
+      }
+
+      if (responseJson.next) {
+        next.disabled = false;
+      } else {
+        next.disabled = true;
+      }
+    });
+};
+
+
+  getCharacters();
+
